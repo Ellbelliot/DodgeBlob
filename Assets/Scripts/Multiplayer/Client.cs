9e4c525050ad700192a807716223474d9154b341
Elliot Clark <elliotkeasbeyclark@gmail.comm>
2021-01-06 15:16:58 -0800

    Properly handes disconnections now

--- Assets/Scripts/Multiplayer/Client.cs
+++ Assets/Scripts/Multiplayer/Client.cs
@@ -16,6 +16,7 @@ public class Client : MonoBehaviour
 	public TCP tcp;
 	public UDP udp;
 
+	private bool isConnected = false;
 	private delegate void PacketHandler(Packet _packet);
 	private static Dictionary<int, PacketHandler> packetHandlers;
 
@@ -39,9 +40,16 @@ public class Client : MonoBehaviour
 		udp = new UDP();
 	}
 
+	private void OnApplicationQuit()
+	{
+		Disconnect();
+	}
+
 	public void ConnectToServer()
 	{
 		InitializeClientData();
+
+		isConnected = true;
 		tcp.Connect();
 	}
 
@@ -103,6 +111,7 @@ public class Client : MonoBehaviour
 				int _byteLength = stream.EndRead(_result);
 				if (_byteLength <= 0)
 				{
+					instance.Disconnect();
 					return;
 				}
 
@@ -116,6 +125,7 @@ public class Client : MonoBehaviour
 			catch (Exception _ex)
 			{
 				Console.WriteLine($"Error receiving TCP data: {_ex}");
+				Disconnect();
 			}
 		}
 
@@ -164,6 +174,16 @@ public class Client : MonoBehaviour
 			}
 
 			return false;
+		}
+
+		private void Disconnect()
+		{
+			instance.Disconnect();
+
+			stream = null;
+			receivedData = null;
+			receiveBuffer = null;
+			socket = null;
 		}
 	}
 
@@ -215,6 +235,7 @@ public class Client : MonoBehaviour
 
 				if (_data.Length < 4)
 				{
+					instance.Disconnect();
 					return;
 				}
 
@@ -222,7 +243,7 @@ public class Client : MonoBehaviour
 			}
 			catch
 			{
-
+				Disconnect();
 			}
 		}
 
@@ -243,6 +264,14 @@ public class Client : MonoBehaviour
 				}
 			});
 		}
+		private void Disconnect()
+		{
+			instance.Disconnect();
+
+			socket = null;
+			endPoint = null;
+		}
+
 	}
 
 	private void InitializeClientData()
@@ -255,5 +284,17 @@ public class Client : MonoBehaviour
 			{ (int)ServerPackets.playerRotation, Clienthandle.PlayerRotation },
 		};
 		Debug.Log("Initialized packets.");
+	}
+
+	private void Disconnect()
+	{
+		if (isConnected)
+		{
+			isConnected = false;
+			tcp.socket.Close();
+			udp.socket.Close();
+
+			Debug.Log("Disconnected from server.");
+		}
 	}
 }
